cb831f6f5c28fa529c08e97d1d2acb1fffd10969
ronexlemon <ronexondimu@gmail.com>
2026-04-29 18:55:42 +0300

    fix:src content

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,14 @@ const nextConfig: NextConfig = {
         headers: [
           {
             key: "Content-Security-Policy",
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline'; connect-src 'self' http://localhost:8080;",
+            value: [
+              "default-src *",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline'",
+              "connect-src *",
+              "style-src 'self' 'unsafe-inline'",
+              "font-src 'self' data:",
+              "img-src * data:",
+            ].join("; "),
           },
         ],
       },
